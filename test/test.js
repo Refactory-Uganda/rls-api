7f903baec2ef149cfe9aa2fec4cfbebffa2e1a2e
Edwin <rugoogamuedwin5@gmail.com>
2023-09-26 11:11:31 +0300

    test.js

--- test/test.js
+++ test/test.js
@@ -3,6 +3,8 @@ const chaiHttp = require("chai-http");
 const Courses = require("../routes/adminAddCoursesRoutes");
 const Login = require("../routes/loginRoutes");
 const Facilitator = require("../routes/facilitatorRoutes")
+const AdminProfile = require("../routes/adminProfileRoutes")
+
 
 
 chai.use(chaiHttp);
@@ -40,6 +42,19 @@ describe("/", () => {
     chai
       .request(Login)
       .get("/login")
+      .end((err, res) => {
+        expect(res.json).to.have.status(200);
+        expect(res.text).to.equal("Hello, Express!");
+        
+      });
+  });
+});
+
+describe("/", () => {
+  it("login route", async () => {
+    chai
+      .request(AdminProfile)
+      .get("/admin/profile")
       .end((err, res) => {
         expect(res.json).to.have.status(200);
         expect(res.text).to.equal("Hello, Express!");

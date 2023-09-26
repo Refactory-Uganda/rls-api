afd4629464b04508b07ba11447d614895a3e6164
Edwin <rugoogamuedwin5@gmail.com>
2023-09-26 10:22:52 +0300

    adminProfile

--- index.js
+++ index.js
@@ -6,21 +6,19 @@ const passport = require("passport");
 const session = require("express-session");
 const port = process.env.PORT || 5000;
 const cors = require("cors");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const LocalStrategy = require("passport-local").Strategy;
 const database = require("./config/database");
 const Login = require("./routes/loginRoutes");
-const Courses = require("./routes/adminAddCoursesRoutes");
-
-
-
-
+const AdminAddCourses = require("./routes/adminAddCoursesRoutes");
+const AdminProfile = require("./routes/adminProfileRoutes");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/", Login);
-app.use("/", Courses);
+app.use("/", AdminAddCourses);
+app.use("/", AdminProfile);
 
 mongoose.connect(database.connect, {
   useNewUrlParser: true,
@@ -34,8 +32,8 @@ db.once("open", () => {
 db.on("error", (err) => {
   console.error(err);
 });
-app.use(express.static("public"))
-app.use( express.static(path.join(__dirname,'public/images')));
+app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public/images")));
 
 // app.use(passport.initialize());
 // app.use(passport.session());
---
+++ routes/adminProfileRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const Profile = require("../controllers/adminProfileController")
+
+router.post("/admin/profile", Profile.post);
+
+router.get("/admin/profile", Profile.get);
+
+router.get("/admin/profile/:id", Profile.get2);
+
+router.put("/admin/profile/:id", Profile.put);
+
+router.delete("/admin/profile/:id", Profile.delete);
+
+module.exports = router;

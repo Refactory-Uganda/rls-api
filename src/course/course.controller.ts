d5569919d1ea915afd82b714ba948523ed836b57
Mable Musiimenta <mmusiimenta@student.refactory.academy>
2024-09-27 17:57:58 +0300

    add update

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,13 +1,29 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Delete } from '@nestjs/common';
+import { Controller, Delete, Put, Body, Post, Get } from '@nestjs/common';
 import { CourseService } from './course.service';
+import { CreateCourseDto } from './dto/create-course.dto';
 
 @Controller('course')
 export class CourseController {
-    constructor(private readonly courseSerivce: CourseService) {}
+    constructor(private readonly courseService: CourseService) {}
+
+    @Post()
+    async create(@Body() createCourseDto: CreateCourseDto) {
+      return this.courseService.create(createCourseDto);
+    }
+
+    @Get()
+    async findAll() {
+      return this.courseService.findAll();
+    }
 
     @Delete()
     async deleteCourse() {
-        return await this.courseSerivce.removeCourse();
+        return await this.courseService.removeCourse();
+    }
+
+    @Put()
+    async updateCourse() {
+        return await this.courseService.updateCourse();
     }
 }
--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,10 +1,29 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Course, CourseDocument } from './course.schema';
+
 
 @Injectable()
 export class CourseService {
+  constructor(@InjectModel(Course.name) private courseModel: Model<CourseDocument>) {}
+
+  async create(createCourseDto: any): Promise<Course> {
+    const newCourse = new this.courseModel(createCourseDto);
+    return newCourse.save();
+  }
+
+  async findAll(): Promise<Course[]> {
+    return this.courseModel.find().exec();
+  }
+
+    updateCourse(): string {
+        return 'Course updated';
+    }
 
     removeCourse():string{
         return 'Course removed';
     }
 }
+

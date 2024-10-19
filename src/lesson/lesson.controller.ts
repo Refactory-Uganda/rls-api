2ad1dc022b55917e468121d1cf2b26bc2fe3a443
Mmadiba724 <matendehussein@gmail.com>
2024-10-19 16:01:41 +0300

    delete lesson endpoint

--- src/lesson/lesson.controller.ts
+++ src/lesson/lesson.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Delete } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { LessonService } from './lesson.service';
 
 @Controller('lesson')
-export class LessonController {}
+@ApiTags('Lessons')
+export class LessonController {
+    constructor(private lessonService: LessonService) {}
+
+    @Delete(':lesson_id')
+    @ApiOperation({summary: 'Delete a Lesson'})
+    async deleteLesson(lesson_id: string) {
+        return this.lessonService.deleteLesson(lesson_id);
+    }
+}
--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -1,4 +1,20 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class LessonService {}
+export class LessonService {
+    constructor(private prisma: PrismaService) {}
+
+
+    async deleteLesson(id:string) {
+        const lesson = await this.prisma.lesson.delete(
+            {
+                where: {
+                    id: id
+                }
+            }
+        )
+        return lesson;
+    }
+}

2942a0466b95d7a1ae93e2179db9a62fe83fdac2
Mable Musiimenta <mablemusii22@gmail.com>
2024-10-23 11:12:30 +0300

    branch

--- src/textnotes/textnotes.controller.ts
+++ src/textnotes/textnotes.controller.ts
@@ -17,6 +17,24 @@ export class NoteController {
     return this.noteService.create(createNoteDto);
   }
 
+  @Put('textnotes_id')
+  @ApiOperation({ summary: 'Update Textnotes' })
+  async update(
+    @Param('textnotes_id') textnotes_id: string,
+    @Body() updateTextnotesDto: UpdateTextnotesDto,
+  ) {
+    return this.textnotesService.updateTextnotes(textnotes_id, updateTextnotesDto);
+  }
+
+  @Patch('textnotes_id')
+  @ApiOperation({ summary: 'Partially Update Textnotes' })
+  async patch(
+    @Param('textnotes_id') textnotes_id: string,
+    @Body() partialUpdateDto: Partial<UpdateTextnotesDto>,
+  ) {
+    return this.textnotesService.patchTextnotes(textnotes_id, partialUpdateDto);
+  }
+
   @Delete(':id')
   @ApiOperation({summary: 'delete a particular note'})
   remove(@Param('id') id: string) {

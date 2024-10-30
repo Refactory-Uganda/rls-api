888d81b5048af4d5422264e56541179cb316e67b
Kanza Jovita <kanzajovita@gmail.com>
2024-10-30 13:16:47 +0300

    refined courses

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -34,13 +34,19 @@ export class CourseController {
     return this.courseService.updateCourse(id, updateCourseDto);
     }
 
-  @Get()
-  @ApiOperation({ summary: 'Get all Courses' })
-  async findAll(
-    @Query('page') page: number =1,
-    @Query('limit') limit: number = 6
-  ) {
-    return await this.courseService.findAll(Number(page),Number(limit));
+  // @Get()
+  // @ApiOperation({ summary: 'Get all Courses' })
+  // async findAll(
+  //   @Query('page') page: number =1,
+  //   @Query('limit') limit: number = 6
+  // ) {
+  //   return await this.courseService.findAll(Number(page),Number(limit));
+  // }
+
+  @Get() 
+  @ApiOperation({summary: 'Get all Courses'})
+  async findAll() {
+    return await this.courseService.findAll(); 
   }
 
 
